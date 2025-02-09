dec7004afcb45c0a7cfb3030d1ba9ada2cce459b
jamilshehab <jamilshehab>
2025-02-09 11:56:08 +0200

    created icons data

--- react-dashboard/src/data/data.ts
+++ react-dashboard/src/data/data.ts
@@ -3,33 +3,35 @@ import { CiShoppingCart } from "react-icons/ci";
 import { FaDollarSign, FaUser } from "react-icons/fa";
 import { FiBarChart2 } from "react-icons/fi";
 import { TbTrendingUp } from "react-icons/tb";
-
+export const ICONS = [
+  FiBarChart2,
+  FaUser,
+  FaDollarSign,
+  CiShoppingCart,
+  TbTrendingUp,
+];
 export const DATA = [
   { id: 0, name: "Overview", icon: FiBarChart2, path: "/" },
   {
     id: 1,
     name: "Products",
-    icon: BiShoppingBag,
     color: "#8B5CF6",
     href: "/products",
   },
-  { id: 2, name: "Users", icon: FaUser, href: "/users" },
+  { id: 2, name: "Users", href: "/users" },
   {
     id: 3,
     name: "Sales",
-    icon: FaDollarSign,
     href: "/sales",
   },
   {
     id: 4,
     name: "Orders",
-    icon: CiShoppingCart,
     href: "/orders",
   },
   {
     id: 5,
     name: "Analytics",
-    icon: TbTrendingUp,
     color: "#3B82F6",
     href: "/analytics",
   },
